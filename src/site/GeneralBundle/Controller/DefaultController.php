e9659912977fb05e367efb952a60ea64d0df0af9
simonhauville <simonhauville@gmail.com>
2015-11-10 17:06:58 +0100

    2eme commit hcv1

---
+++ src/site/GeneralBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace site\GeneralBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('siteGeneralBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/site/GeneralBundle/siteGeneralBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace site\GeneralBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class siteGeneralBundle extends Bundle
+{
+}
